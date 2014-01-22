925a9e60b1b3e7789ba792fa4bf43397e142d6f5
YOwatari <yyutao88@gmail.com>
2014-01-23 00:06:40 +0900

    12/72ページ途中

---
+++ src/example/hellofragment/MainActivity.java
@@ -0,0 +1,38 @@
+package example.hellofragment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	private Button mSearchBtn;
+	private EditText mEditText;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		mSearchBtn = (Button) findViewById(R.id.search_btn);
+		mEditText = (EditText) findViewById(R.id.keyword);
+		
+		mSearchBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String keyword = mEditText.getText().toString();
+				if (!TextUtils.isEmpty(keyword)) {
+					Intent intent = new Intent();
+					intent.putExtra(Intent.EXTRA_TEXT, keyword);
+					startActivity(intent);
+				}
+			}
+		});
+	}
+}
