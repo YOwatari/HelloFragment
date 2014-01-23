e1c45a87fe2a6a556f99330b751875d0c9259142
YOwatari <yyutao88@gmail.com>
2014-01-23 20:13:03 +0900

    apacheのhttpclient．プロクシうざい

--- src/example/hellofragment/MainActivity.java
+++ src/example/hellofragment/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,7 +27,7 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				String keyword = mEditText.getText().toString();
 				if (!TextUtils.isEmpty(keyword)) {
-					Intent intent = new Intent();
+					Intent intent = new Intent(MainActivity.this, ResultActivity.class);
 					intent.putExtra(Intent.EXTRA_TEXT, keyword);
 					startActivity(intent);
 				}
